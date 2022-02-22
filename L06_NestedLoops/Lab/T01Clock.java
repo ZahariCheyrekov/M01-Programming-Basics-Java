ac26ce9a93e269fddcc4742d575f00c1134b6a44
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-22 18:23:46 +0200

    Update T01Clock.java

--- L06_NestedLoops/Lab/T01Clock.java
+++ L06_NestedLoops/Lab/T01Clock.java
@@ -5,6 +5,7 @@ public class T01Clock {
 
         for (int h = 0; h < 24; h++) {
             for (int m = 0; m < 60; m++) {
+      
                 System.out.printf("%d:%d%n", h, m);
             }
         }
