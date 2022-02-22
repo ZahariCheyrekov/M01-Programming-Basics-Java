617f7bf37ea52608fd0450686996390a976a8af4
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-22 18:24:35 +0200

    Update T03Combinations.java

--- L06_NestedLoops/Lab/T03Combinations.java
+++ L06_NestedLoops/Lab/T03Combinations.java
@@ -5,6 +5,7 @@ public class T03Combinations {
 
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
+             
                 int total = i * j;
                 System.out.printf("%d * %d = %d%n", i, j, total);
             }
