3efd3227b99900aa18e833f16f14faa44dd42560
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-22 18:24:20 +0200

    Update T02MultiplicationTable.java

--- L06_NestedLoops/Lab/T02MultiplicationTable.java
+++ L06_NestedLoops/Lab/T02MultiplicationTable.java
@@ -6,6 +6,7 @@ public class T02MultiplicationTable {
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
                 int total = i * j;
+            
                 System.out.printf("%d * %d = %d%n", i, j, total);
             }
         }
