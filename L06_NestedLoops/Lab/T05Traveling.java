b4a63720adfa1fea1a832e9f38a28fe8837b1aa7
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-22 18:25:26 +0200

    Update T05Traveling.java

--- L06_NestedLoops/Lab/T05Traveling.java
+++ L06_NestedLoops/Lab/T05Traveling.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class T05Traveling {
     public static void main(String[] args) {
+      
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
