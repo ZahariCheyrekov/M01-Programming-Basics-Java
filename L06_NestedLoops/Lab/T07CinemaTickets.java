09ff1f3d51b59961443a269133eb050aa527c721
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-02-22 18:26:13 +0200

    Update T07CinemaTickets.java

--- L06_NestedLoops/Lab/T07CinemaTickets.java
+++ L06_NestedLoops/Lab/T07CinemaTickets.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class T07CinemaTickets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+      
         int totalTickets = 0;
         int studentsTickets = 0;
         int standardTickets = 0;
@@ -16,6 +17,7 @@ public class T07CinemaTickets {
             int movieTickets = 0;
 
             String type;
+           
             while (movieTickets < freeSeats && !"End".equals(type = scanner.nextLine())) {
                 if (type.equals("student")) {
                     studentsTickets++;
@@ -25,14 +27,17 @@ public class T07CinemaTickets {
                     standardTickets++;
                     totalTickets++;
                     movieTickets++;
+              
                 } else if (type.equals("kid")) {
                     kidTickets++;
                     totalTickets++;
                     movieTickets++;
                 }
             }
+         
             System.out.printf("%s - %.2f%% full.%n", movie, movieTickets * 100.0 / freeSeats);
         }
+      
         System.out.printf("Total tickets: %d%n", totalTickets);
         System.out.printf("%.2f%% student tickets.%n", studentsTickets * 100.0 / totalTickets);
         System.out.printf("%.2f%% standard tickets.%n", standardTickets * 100.0 / totalTickets);
