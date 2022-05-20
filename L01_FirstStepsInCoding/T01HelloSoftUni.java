3f1049797b4d64c9e37fa9d4cc9c62e9ee8ea79c
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-20 18:52:12 +0300

    Update T01HelloSoftUni.java

--- L01_FirstStepsInCoding/T01HelloSoftUni.java
+++ L01_FirstStepsInCoding/T01HelloSoftUni.java
@@ -2,7 +2,6 @@ package M01_ProgrammingBasics.L01_FirstStepsInCoding;
 
 public class T01HelloSoftUni {
     public static void main(String[] args) {
-
         System.out.println("Hello SoftUni");
     }
 } 
