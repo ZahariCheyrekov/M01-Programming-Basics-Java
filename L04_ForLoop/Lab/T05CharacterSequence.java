bc3f6ccfb3baf1e94ba48d658aadf091ba326352
Zahari Cheyrekov <95768526+ZahariCheyrekov@users.noreply.github.com>
2022-05-21 07:14:50 +0300

    Update T05CharacterSequence.java

--- L04_ForLoop/Lab/T05CharacterSequence.java
+++ L04_ForLoop/Lab/T05CharacterSequence.java
@@ -6,7 +6,8 @@ public class T05CharacterSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
-        for (int i = 0; i < s.length(); i++) {
+       
+	for (int i = 0; i < s.length(); i++) {
             char letter = s.charAt(i);
             System.out.println(letter);
         }
